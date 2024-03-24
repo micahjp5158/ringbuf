045515c311db54fdf78039cd4a17b8cc208997d9
Micah Perry <micahjp5158@gmail.com>
2024-03-23 21:36:37 -0400

    Remove void dereference
    Removes a void pointer dereference. Replaces with some math with
    positions and element size to get proper address.

--- src/ringbuf.c
+++ src/ringbuf.c
@@ -76,7 +76,7 @@ RingBuf_Status_t ringbuf_get(RingBuf_Handle_t *ringbuf, void *data){
   }
 
   // Copy data from current head to the destination
-  memcpy(data, &(ringbuf->buf[ringbuf->head * ringbuf->elem_size]), ringbuf->elem_size);
+  memcpy(data, (ringbuf->buf + (ringbuf->tail * ringbuf->elem_size)), ringbuf->elem_size);
 
   // If head is at final element, go to beginning
   // Othersize, wrap back to the beginning
@@ -111,7 +111,7 @@ RingBuf_Status_t ringbuf_put(RingBuf_Handle_t *ringbuf, const void *data)
   }
 
   // Copy the data into the buffer at the new tail position
-  memcpy(&(ringbuf->buf[ringbuf->tail * ringbuf->elem_size]), data, ringbuf->elem_size);
+  memcpy((ringbuf->buf + (ringbuf->tail * ringbuf->elem_size)), data, ringbuf->elem_size);
 
   // Increase number of elements
   ringbuf->num_elements++;
