30017da6df91c11e397a90994614fa8f8adb233a
Micah Perry <micahjp5158@gmail.com>
2024-03-23 21:06:18 -0400

    Add basic ringbuf struct and functions
    Adds a basic ringbuf struct with initializer, getter, and putter
    functions.

---
+++ inc/ringbuf.h
@@ -0,0 +1,91 @@
+/**
+ ********************************************************************************
+ * @file    ringbuf.h
+ * @author  micahjp5158
+ * @date    3-23-2024
+ * @brief   Ring buffer implementation in C
+ ********************************************************************************
+ */
+
+#ifndef __RINGBUF_H__
+#define __RINGBUF_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/************************************
+ * INCLUDES
+ ************************************/
+#include <stdbool.h>
+#include <stddef.h>
+
+/************************************
+ * MACROS AND DEFINES
+ ************************************/
+
+/************************************
+ * TYPEDEFS
+ ************************************/
+typedef enum ringbuf_status {
+  RINGBUF_STATUS_OK,
+  RINGBUF_STATUS_NULL_PTR,
+  RINGBUF_STATUS_BUF_SIZE_NOT_PWR_OF_2,
+  RINGBUF_STATUS_EMPTY,
+  RINGBUF_STATUS_FULL,
+  RINGBUF_STATUS_NUM
+} RingBuf_Status_t;
+
+/* Struct members should not be access directly - only use init, get, and put handlers*/
+typedef struct {
+  void *buf;                    // Ringbuffer data buffer
+  size_t elem_size;             // Size of one element in the buffer
+  size_t buf_size;              // Number of elements in the buffer
+  volatile size_t head;         // First element of the ringbuffer
+  volatile size_t tail;         // Last element of the ringbuffer
+  volatile size_t num_elements; // Number of elements in the ringbuffer
+} RingBuf_Handle_t;
+
+/************************************
+ * EXPORTED VARIABLES
+ ************************************/
+
+/************************************
+ * GLOBAL FUNCTION PROTOTYPES
+ ************************************/
+
+/**
+ * @brief Initialize a RingBuf handler with the specified buffer parameters.
+ * @param ringbuf: Ringbuf handler.
+ * @param buf: Pre-allocated buffer for the ringbuffer.
+ * @param elem_size: Size of each element in the buffer.
+ * @param buf_size: Number of elements in the buffer.
+ * @return: RINGBUF_STATUS_OK if successfully created, error code otherwise.
+ ********************************************************************************/
+RingBuf_Status_t ringbuf_init(RingBuf_Handle_t *ringbuf, void *buf, size_t elem_size, size_t buf_size);
+
+/**
+ * @brief Get (and remove) the oldest element in the ring buffer.
+ * @param ringbuf: Ringbuf handler.
+ * @param data: If get is successful, the element's data will be written here.
+ * @return: RINGBUF_STATUS_OK if successfully read, error code otherwise.
+ * @note: Contains a critical section for the ring buffer. Assumes the caller
+ *        holds the appropriate mutex or otherwise prevents race conditions.
+ ********************************************************************************/
+RingBuf_Status_t ringbuf_get(RingBuf_Handle_t *ringbuf, void *data);
+
+/**
+ * @brief Insert a new element to the end of a ring buffer.
+ * @param ringbuf: Ringbuf handler.
+ * @param data: Element to insert into the buffer.
+ * @return: RINGBUF_STATUS_OK if successfully inserted, error code otherwise.
+ * @note: Contains a critical section for the ring buffer. Assumes the caller
+ *        holds the appropriate mutex or otherwise prevents race conditions.
+ ********************************************************************************/
+RingBuf_Status_t ringbuf_put(RingBuf_Handle_t *ringbuf, const void *data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __RINGBUF_H__
---
+++ src/ringbuf.c
@@ -0,0 +1,120 @@
+/**
+ ********************************************************************************
+ * @file    ringbuf->c
+ * @author  micahjp5158
+ * @date    3-23-2024
+ * @brief   Ring buffer implementation in C
+ ********************************************************************************
+ */
+
+/************************************
+ * INCLUDES
+ ************************************/
+#include "ringbuf->h"
+
+#include <stdbool.h>
+#include <string.h>
+
+/************************************
+ * EXTERN VARIABLES
+ ************************************/
+
+/************************************
+ * PRIVATE MACROS AND DEFINES
+ ************************************/
+
+/************************************
+ * PRIVATE TYPEDEFS
+ ************************************/
+
+/************************************
+ * STATIC VARIABLES
+ ************************************/
+
+/************************************
+ * GLOBAL VARIABLES
+ ************************************/
+
+/************************************
+ * STATIC FUNCTION PROTOTYPES
+ ************************************/
+
+/************************************
+ * STATIC FUNCTIONS
+ ************************************/
+
+/************************************
+ * GLOBAL FUNCTIONS
+ ************************************/
+RingBuf_Status_t ringbuf_init(RingBuf_Handle_t *ringbuf, void *buf, size_t elem_size, size_t buf_size)
+{
+  // Verify the buffer exists
+  if (buf == NULL)
+  {
+    return RINGBUF_STATUS_NULL_PTR;
+  }
+
+  // Clear the buffer
+  memset(buf, 0, ringbuf->elem_size * ringbuf->buf_size);
+
+  // Set / initialize ringbuffer struct members
+  ringbuf->buf = buf;
+  ringbuf->elem_size = elem_size;
+  ringbuf->buf_size = buf_size;
+  ringbuf->head = 0;
+  ringbuf->tail = 0;
+  ringbuf->num_elements = 0;
+
+  return RINGBUF_STATUS_OK;
+}
+
+RingBuf_Status_t ringbuf_get(RingBuf_Handle_t ringbuf, void *data){
+  // Make sure buffer is not empty
+  if (ringbuf->num_elements == 0)
+  {
+    return RINGBUF_STATUS_EMPTY;
+  }
+
+  // Copy data from current head to the destination
+  memcpy(data, ringbuf->buf[ringbuf->head * ringbuf->elem_size], ringbuf->elem_size);
+
+  // If head is at final element, go to beginning
+  // Othersize, wrap back to the beginning
+  if(ringbuf->head == ringbuf->buf_size)
+  {
+    ringbuf->head = 0;
+  } else {
+    ringbuf->head++;
+  }
+
+  // Decrease number of elements
+  ringbuf->num_elements--;
+
+  return RINGBUF_STATUS_OK;
+}
+
+RingBuf_Status_t ringbuf_put(RingBuf_Handle_t ringbuf, const void *data)
+{
+  // Make sure buffer is not full
+  if (ringbuf->num_elements >= buf_size)
+  {
+    return RINGBUF_STATUS_FULL;
+  }
+
+  // If tail is at final element, go to beginning
+  // Othersize, wrap back to the beginning
+  if(ringbuf->tail == ringbuf->buf_size)
+  {
+    ringbuf->tail = 0;
+  } else {
+    ringbuf->tail++;
+  }
+
+  // Copy the data into the buffer at the new tail position
+  memcpy(ringbuf->buf[ringbuf->tail * ringbuf->elem_size], data, ringbuf->elem_size);
+
+  // Increase number of elements
+  ringbuf->num_elements++;
+
+  return RINGBUF_STATUS_OK;
+}
